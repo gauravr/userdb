30cd11e44869ad131b566df0a4d3e6262ad47e14
Gaurav <gaurav@scroll.in>
2016-11-04 01:20:21 +0530

    Added get_attribute, update_attribute & exists methods

--- userdb/sessions.py
+++ userdb/sessions.py
@@ -23,14 +23,28 @@ def create():
     return sid
 
 
-def get(sid):
+def exists(sid):
 
-    s_values = rconn.hgetall(skey(sid))
-    if not s_values:
-        return {}
+    return rconn.exists(sid)
 
-    data = {k.decode('ascii'):pickle.loads(v) for k,v in s_values.items()}
+
+def get(sid, keys=[]):
+
+    if keys:
+        s_values = rconn.hmget(skey(sid), keys)
+        data = {k:pickle.loads(v) if v else v for k,v in zip(keys, s_values)}
+    else:
+        s_values = rconn.hgetall(skey(sid))
+        if s_values is None:
+            s_values = {}
+        data = {k.decode('ascii'):pickle.loads(v) for k,v in s_values.items()}
     return data
+
+
+def get_attribute(sid, attribute):
+
+    value = rconn.hget(skey(sid), attribute)
+    return pickle.loads(value) if value else None
 
 
 def destroy(sid):
@@ -44,6 +58,14 @@ def update(sid, mod_data):
     key = skey(sid)
     mod_data = {k:pickle.dumps(v) for k,v in mod_data.items() if k in settings.SESSION_KEYS}
     rconn.hmset(key, mod_data)
+    rconn.expire(key, settings.TTL)
+    return True
+
+
+def update_attribute(sid, attribute, value):
+
+    key = skey(sid)
+    rconn.hset(key, attribute, pickle.dumps(value))
     rconn.expire(key, settings.TTL)
     return True
 
